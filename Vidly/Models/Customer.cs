a18a94ae33b5540730427852b59f5333a3f67399
Rogerio <rogerio_dfb@hotmail.com>
2019-05-06 23:00:15 -0300

    Update To Customers Table

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -17,5 +17,6 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? Birthdate { get; set; }
     }
 }
