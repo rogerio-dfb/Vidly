40e51ee6193b58280a76c7c2af862c0e498645db
Rogerio <rogerio_dfb@hotmail.com>
2019-05-06 23:24:11 -0300

    Add Genre and Update Movie Model

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,31 +11,35 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            List<Movie> movies = GetMovies();
+            List<Movie> movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
             return View(movie);
-        }
-
-        private List<Movie> GetMovies()
-        {
-            return new List<Movie>()
-            {
-                new Movie() {Id=1, Name="Shrek!"},
-                new Movie() {Id=2,Name="Wall-e"}
-            };
         }
     }
 }
